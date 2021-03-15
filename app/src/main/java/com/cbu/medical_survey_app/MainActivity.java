621bdfbd9bdd0adcd470f0d5e3a7a707bf4edd5c
leejaeseung <wasd222@naver.com>
2021-03-15 18:02:29 +0900

    버튼 drawable 추가

--- app/src/main/java/com/cbu/medical_survey_app/MainActivity.java
+++ app/src/main/java/com/cbu/medical_survey_app/MainActivity.java
@@ -1,6 +1,12 @@
 package com.cbu.medical_survey_app;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -8,6 +14,8 @@ import androidx.fragment.app.FragmentTransaction;
 import com.cbu.medical_survey_app.fragments.Last_Fragment;
 
 public class MainActivity extends FragmentActivity {
+
+    private InputMethodManager imm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +25,28 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.last_page);
 
+        imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.add(R.id.survey_content, new Last_Fragment());
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        // 키보드 바깥 클릭 시 키보드 닫는 로직
+        View v = getCurrentFocus();
+
+        if(v != null && (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_MOVE) && v instanceof EditText && !v.getClass().getName().startsWith("android.webkit.")){
+            int[] scrcoords = new int[2];
+            v.getLocationOnScreen(scrcoords);
+            float x = event.getRawX() + v.getLeft() - scrcoords[0];
+            float y = event.getRawY() + v.getTop() - scrcoords[1];
+
+            if(x < v.getLeft() || x > v.getRight() || y < v.getTop() || y > v.getBottom())
+                ((InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
